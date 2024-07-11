6356e50848626a5a50140b738ba54168f6a18688
Vasan <kjvasan28503@gmail.com>
2024-07-11 02:54:25 -0400

    msg

--- javascript/health_article.js
+++ javascript/health_article.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-var url = './health.json';
+var url = './health_article.json';
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 var articles = xhr.response.articles;
